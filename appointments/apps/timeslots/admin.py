3586788141d2736a063e43457df620accf303680
Marcel Oomens <Marcel.Oomens@cromido.org>
2014-07-05 22:11:40 +0800

    Cumulative updates
    Cumulative updates, end of day 1
    
    Signed-off-by: Marcel Oomens <Marcel.Oomens@cromido.org>

--- appointments/apps/timeslots/admin.py
+++ appointments/apps/timeslots/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from jsonfield import JSONCharField
+from django.forms import Textarea
+
+from .models import ConstraintSet, Constraint, Definition
+
 # Register your models here.
+
+class ConstraintAdmin(admin.TabularInline):
+
+    extra = 0
+    fields = ('name', 'slug', 'enabled',)
+    model = Constraint
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class ConstraintSetAdmin(admin.ModelAdmin):
+
+    inlines = (ConstraintAdmin,)
+    fields = ('name', 'slug', 'enabled',)
+    list_display = ('name', 'slug', 'enabled',)
+    prepopulated_fields = {'slug': ('name',)}
+    
+
+class DefinitionAdmin(admin.ModelAdmin):
+    # list_display = ('start_date', 'constraint', 'kind', 'encoding')
+    # readonly_fields = ('end_date', 'enabled')
+    list_display = ('constraint',)
+    
+    
+admin.site.register(ConstraintSet, ConstraintSetAdmin)
+admin.site.register(Definition, DefinitionAdmin)
--- appointments/apps/timeslots/models.py
+++ appointments/apps/timeslots/models.py
@@ -5,6 +5,54 @@ from jsonfield import JSONField
 
 # Create your models here.
 
+#  TO DO
+# - Generalize constraints
+# - Allow multiple constraints per Definition
+
+class ConstraintSet (models.Model):
+
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=64, unique=True)
+    
+    enabled = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Location")
+        verbose_name_plural =  _("Locations")
+    
+    def __str__(self):
+        return  "<ConstraintSet/Location: %s>" % (self.name)
+        
+    # def choices(self):
+    #     return [(value.slug, value.name) for value in self.values.all() if value.enabled]
+            
+            
+class Constraint (models.Model):
+
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(max_length=64, unique=True)
+
+    key = models.ForeignKey(ConstraintSet, related_name='values')
+
+    enabled = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("City")
+        verbose_name_plural =  _("Cities")
+        
+    def __str__(self):
+        return "<Constraint/Location: %s, %s>" % (self.key.name, self.name)
+
+
 class Definition (models.Model):
 
+    constraint = models.ForeignKey(Constraint)
+    
     json = JSONField(_("Definition"))
+    
+    class Meta:
+        verbose_name = _("Timeslots")
+        verbose_name_plural = _("Timeslots")
+        
+    def __str__(self):
+        return "<Definition id=%s>" % (self.pk)
