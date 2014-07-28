5c8718345100c2c72a41a465c037c896849b457a
Marcel Oomens <Marcel.Oomens@cromido.org>
2014-07-28 16:06:32 +0800

    Set-up staging environment
    bug in settings.py
    
    Signed-off-by: Marcel Oomens <Marcel.Oomens@cromido.org>

--- appointments/settings/prod.py
+++ appointments/settings/prod.py
@@ -4,7 +4,7 @@ from .common import *
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default':  dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
